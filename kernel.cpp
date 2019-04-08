5eaec11c22fc6b08681a437298ba8c2670e7caac
Justin Nguyen <justinnuwin@gmail.com>
2019-04-07 23:47:37 -0700

    Remove debugging code

--- kernel.cpp
+++ kernel.cpp
@@ -1,26 +1,11 @@
 #include "kernel.h"
 
-// #include "vga.hpp"
+#include "vga.hpp"
 #include <stdint.h>
 
-/*
-inline void
-clear() {
-    int count = 80 * 25;
-    uint16_t *buffer = (uint16_t *)0xb8000;
-    while (count--) {
-        *buffer = 0x004a;
-        buffer++;
-    }
-}
-*/    
-
 void kernel_main(void) {
-    // VGA vga;
-    // vga.clear();
-    uint16_t *buffer = (uint16_t *)0xb8000;
-    *buffer = 0x004a;
-    //clear();
+    VGA vga;
+    vga.clear();
     while (1) {
     }
 }
