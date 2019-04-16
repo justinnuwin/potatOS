d2e863105cc33f8ec8ccda3ad27b566e5d2ab2ed
Justin Nguyen <justinnuwin@gmail.com>
2019-04-16 01:27:36 -0700

    Move inline functions to header

--- asm_functions.h
+++ asm_functions.h
@@ -3,7 +3,30 @@
 #ifndef ASM_FUNCTIONS_H
 #define ASM_FUNCTIONS_H
 
-inline void outb(uint16_t port, uint8_t val);
-inline uint8_t inb(uint16_t port);
+/*
+inline void lgdt(void *base, uint16_t size) {
+    static struct GDTR {
+        uint16_t length;
+        void *base;
+    } __attribute__ ((packed));
+
+    GDTR.length size;
+    GDTR.base = base;
+
+    asm ("lgdt %0" : : "m"(GDTR));
+}
+*/
+
+inline void outb(uint16_t port, uint8_t val) {
+    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile ("intb %1, %0"
+                    : "=a"(ret)
+                    : "Nd"(port));
+    return ret;
+}
 
 #endif
